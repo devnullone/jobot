c97a581f5cd6325d7511cbc21200d88cfd3bfba8
KODZO A. M. Mawuena <nashmichel70@gmail.com>
2021-10-20 13:43:38 +0000

    Add alljobs.py
    Ce fichier est un script qui récupère tout les liens des annonces de job sur le site (https://www.emploi.tg/). Les liens sont ajoutés dans le fichier alljobs.txt

---
+++ alljobs.py
@@ -0,0 +1,25 @@
+import requests
+from bs4 import BeautifulSoup
+import time
+
+alllinks =  []
+for i in range(4):
+    url = "https://www.emploi.tg/recherche-jobs-togo?page=" + str(i)
+
+    response = requests.get(url)
+    if response.ok:
+        last = BeautifulSoup(response.text, 'html.parser')
+        links = last.select('a[href*="offre-emploi-togo"]') #Jobs Links on pages
+        for x in links:
+            y = x['href'] #Get value of attrib 'href'
+            alllinks.append("https://www.emploi.tg" + y)
+        alllinks_ = set(alllinks) # list -> set
+        alllinks = list(alllinks_) # set -> list
+        time.sleep(3)
+
+#Write all Links in .txt file
+with open('alljobs.txt', 'w') as file:
+    for link in alllinks:
+        file.write(link + '\n')
+
+
